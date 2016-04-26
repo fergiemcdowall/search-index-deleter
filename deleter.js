be9a8ced6f727ba36c06dce17d0f95ef8ebe8899
Fergie McDowall <fergusmcdowall@gmail.com>
2016-04-26 11:12:23 +0200

    some tidying up

--- deleter.js
+++ deleter.js
@@ -5,117 +5,158 @@ var _flatten = require('lodash.flatten');
 var _remove = require('lodash.remove')
 var _uniq = require('lodash.uniq');
 var a = require('async');
-var skeleton = require('log-skeleton');
 
-module.exports = function (givenOptions, callback) {
 
-  getOptions(givenOptions, function(err, options) {
-  
-    var log = skeleton((options) ? options.log : undefined);
-    var Deleter = {};
-
-    Deleter.deleteBatch = function (deleteBatch, callbacky) {
-      var numberDocsToDelete = 0;
-      if (!options.deletable) {
-        return callbacky(new Error('this index is non-deleteable- set "deletable: true" in startup options'))
-      }
-      if (!Array.isArray(deleteBatch)) {
-        deleteBatch = [deleteBatch]
-      }
-      a.mapLimit(deleteBatch, 5, function (docID, callback) {
-        options.indexes.get('DELETE-DOCUMENT￮' + docID, function (err, keys) {
-          if (err) {
-            if (err.name == 'NotFoundError') {
-              return callback(null, []);
-            }
-          }
-          else
-            numberDocsToDelete++;
-          return callback(err, keys);
-        });
-      }, function (err, results) {
-        if (!err) {
-          results = _flatten(results).sort()
-          results = _uniq(results)
-          a.mapLimit(
-            // _(results).flatten().sort().uniq(true).value(),
-            results,
-            5,
-            function (key, callback) {
-              options.indexes.get(key, function (err, value) {
-                var dbInstruction = {type: 'put',
-                                     key: key};
-                var recalibratedValue = [];
-                if (key.substring(0, 2) == 'DF') {
-                  recalibratedValue = _remove(value, function (n) {
-                    return (deleteBatch.indexOf(n + '') == -1);
-                  }).sort();
-                }
-                else if (key.substring(0, 2) == 'TF') {
-                  recalibratedValue = value.filter(function (item) {
-                    return (deleteBatch.indexOf(item[1]) == -1);
-                  }).sort(function (a, b) {
-                    return b[0] - a[0];
-                  });
-                }
-                if (recalibratedValue.length == 0)
-                  dbInstruction.type = 'del';
-                else
-                  dbInstruction.value = recalibratedValue;
-                callback(err, dbInstruction);
-              });
-            }, function (err, dbInstructions) {
-              deleteBatch.forEach(function (docID) {
-                dbInstructions.push({
-                  type: 'del',
-                  key: 'DELETE-DOCUMENT￮' + docID
-                });
-              });
-              options.indexes.batch(dbInstructions, function (err) {
-                if (err) log.warn('Ooops!', err);
-                else log.info('batch indexed!');
-                // make undefined error null
-                if (typeof err == undefined) err = null;
-                options.indexes.get('DOCUMENT-COUNT', function (err, value) {
-                  var docCount
-                  if (err)  //no DOCUMENT-COUNT set- first indexing
-                    docCount = 0
-                  else
-                    docCount = (+value - (numberDocsToDelete))
-                  options.indexes.put('DOCUMENT-COUNT',
-                                      docCount,
-                                      function (errr) {
-                                        return callbacky(err);
-                                      });
-                });
-              });
-            });
-        }
-        else return callbacky(err);
-      });
-    };
-
-    Deleter.flush = function (callback) {
-      var deleteOps = []
-      options.indexes.createKeyStream({gte: '0', lte: '￮'})
-        .on('data', function (data) {
-          deleteOps.push({type: 'del', key: data})
-        })
-        .on('error', function (err) {
-          log.error(err, ' failed to empty index')
-        })
-        .on('end', function () {
-          options.indexes.batch(deleteOps, callback)
-        })
+// Constructor
+module.exports = function (givenOptions, constructorCallback) {
+  var options = {}
+  a.series([
+    // munge given options into default options, open new DB if necessary
+    function(callback) {
+      getOptions(givenOptions, function(err, theseOptions) {
+        options = theseOptions
+        return callback(err)
+      })
+    },
+    // use options to create a new DB object
+    function(callback) {
+      getDeleter(options, function(err, Deleter) {
+        return callback(err, Deleter)
+      })
     }
-
-//    return Deleter;
-    return callback(null, Deleter)
+  ], function (err, results) {
+    return constructorCallback(err, results[1])
   })
-};
+
+}
+
+// Define API calls
+var getDeleter = function (options, callback) {
+  var Deleter = {}
+  // Delete all docs with the IDs found in deleteBatch
+  Deleter.deleteBatch = function (deleteBatch, APICallback) {
+    tryDeleteBatch (options, deleteBatch, function(err) {
+      return APICallback(err)
+    })
+  }
+  // Flush the db (delete all entries)
+  Deleter.flush = function (APICallback) {
+    flush(options, function(err) {
+      return APICallback(err)
+    })
+  }
+  return callback(null, Deleter)  
+}
 
 
+var deleteThisBatch = function (options, numberDocsToDelete, deleteBatch, results, callbacky) {
+  results = _flatten(results).sort()
+  results = _uniq(results)
+  a.mapLimit(
+    results,
+    5,
+    function (key, callback) {
+      options.indexes.get(key, function (err, value) {
+        var dbInstruction = {
+          type: 'put',
+          key: key
+        };
+        var recalibratedValue = [];
+        if (key.substring(0, 2) == 'DF') {
+          recalibratedValue = _remove(value, function (n) {
+            return (deleteBatch.indexOf(n + '') == -1);
+          }).sort();
+        }
+        else if (key.substring(0, 2) == 'TF') {
+          recalibratedValue = value.filter(function (item) {
+            return (deleteBatch.indexOf(item[1]) == -1);
+          }).sort(function (a, b) {
+            return b[0] - a[0];
+          });
+        }
+        if (recalibratedValue.length == 0)
+          dbInstruction.type = 'del';
+        else
+          dbInstruction.value = recalibratedValue;
+        callback(err, dbInstruction);
+      });
+    }, function (err, dbInstructions) {
+      deleteBatch.forEach(function (docID) {
+        dbInstructions.push({
+          type: 'del',
+          key: 'DELETE-DOCUMENT￮' + docID
+        });
+      });
+      options.indexes.batch(dbInstructions, function (err) {
+        if (err) options.log.warn('Ooops!', err);
+        else options.log.info('batch indexed!');
+        // make undefined error null
+        if (typeof err == undefined) err = null;
+        options.indexes.get('DOCUMENT-COUNT', function (err, value) {
+          var docCount
+          if (err)  //no DOCUMENT-COUNT set- first indexing
+            docCount = 0
+          else
+            docCount = (+value - (numberDocsToDelete))
+          options.indexes.put('DOCUMENT-COUNT',
+                              docCount,
+                              function (errr) {
+                                return callbacky(err);
+                              });
+        });
+      });
+    }); 
+}
 
+
+// Try to delete the batch, throw errors if config, or formatting prevents deletion
+var tryDeleteBatch = function (options, deleteBatch, callbacky) {
+  var numberDocsToDelete = 0;
+  if (!options.deletable) {
+    return callbacky(new Error('this index is non-deleteable- set "deletable: true" in startup options'))
+  }
+  if (!Array.isArray(deleteBatch)) {
+    deleteBatch = [deleteBatch]
+  }
+  a.mapLimit(deleteBatch, 5, function (docID, callback) {
+    options.indexes.get('DELETE-DOCUMENT￮' + docID, function (err, keys) {
+      if (err) {
+        if (err.name == 'NotFoundError') {
+          return callback(null, []);
+        }
+      }
+      else
+        numberDocsToDelete++;
+      return callback(err, keys);
+    });
+  }, function (err, results) {
+    if (err) {
+      return callbacky(err)
+    } else {
+      deleteThisBatch(options, numberDocsToDelete, deleteBatch, results, function(err) {
+        callbacky(err)
+      })
+    }
+  });
+}
+
+// Remove all keys in DB
+var flush = function(options, callback) {
+  var deleteOps = []
+  options.indexes.createKeyStream({gte: '0', lte: '￮'})
+    .on('data', function (data) {
+      deleteOps.push({type: 'del', key: data})
+    })
+    .on('error', function (err) {
+      log.error(err, ' failed to empty index')
+      return callback(err)
+    })
+    .on('end', function () {
+      options.indexes.batch(deleteOps, callback)
+    })
+}
+
+// munge given options with default options
 var getOptions = function(givenOptions, callbacky) {
   const async = require('async')
   const bunyan = require('bunyan')
